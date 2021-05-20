eadf7c2a815e41e594cd26fa9f622574e01a9558
Plane <crystal@panix.com>
2021-05-20 06:12:56 -0700

    Someday, this will grow up into a pkg with docs

--- info.rkt
+++ info.rkt
@@ -1,4 +1,16 @@
 #lang info
-(define version "0.1")
-(define deps '("threading-lib"
-               "struct-define"))
+
+(define collection "random-generators")
+(define pkg-authors '("crystal@panix.com"))
+(define version "0.2")
+(define scribblings
+  '(("random-generators.scrbl" () (library) "random-generators")))
+(define pkg-desc "Various random number generators")
+(define deps '("base"
+			   "threading-lib"
+			   "struct-define"))
+(define build-deps '("rackunit-lib"
+					 "racket-doc"
+					 "scribble-lib"))
+(define compile-omit-paths '("examples"))
+
---
+++ random-generators.scrbl
@@ -0,0 +1,8 @@
+#lang scribble/manual
+
+@title{Pseudo-Random Number Generators}
+@defmodule[random-generators]
+@author[@author+email["Plane" "crystal@panix.com"]]
+
+Sample text
+
