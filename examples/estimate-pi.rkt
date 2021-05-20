7946579af6ea68ba3539ab3a86f014189e697a6b
Plane <crystal@panix.com>
2021-05-20 00:43:32 -0700

    use for/count

--- examples/estimate-pi.rkt
+++ examples/estimate-pi.rkt
@@ -8,6 +8,14 @@
 
 (require "../main.rkt")
 
+(define-syntax-rule 
+  (for/count (for-clause ...)
+     body-or-break ...
+     body)
+  (for/sum (for-clause ...)
+     body-or-break ...
+     (if body 1 0)))
+
 (define (in-quarter-circle? x y)
   (< (+ (* x x)
         (* y y))
@@ -15,26 +23,20 @@
 
 (define (count-dots-in-quarter-circle trial-num)
   (define rng (xoshiro128++/current-time))
-  (for/fold ([dots-in 0])
-            ([_ trial-num])
-    (if (in-quarter-circle? (random-real! rng)
-                            (random-real! rng))
-      (add1 dots-in)
-      dots-in)))
+  (for/count ([_ trial-num])
+    (in-quarter-circle? (random-real! rng)
+                        (random-real! rng))))
 
 (define (estimate-pi trial-num)
   (define dots-in-quarter-circle
     (count-dots-in-quarter-circle trial-num))
-
   (define ratio-in-quarter-circle
     (/ dots-in-quarter-circle trial-num))
-
   (define ratio-in-circle
     (* 4 ratio-in-quarter-circle))
-
   (exact->inexact ratio-in-circle))
 
 (module+ main
-  (define trial-num 10000000)
+  (define trial-num 100000)
   (estimate-pi trial-num))
 
